298a047c3214b9493912eca23aed41eac375d3c3
ClayGuess <guessclay@Yahoo.com>
2020-05-20 18:00:53 -0400

    styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,22 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="homewrapper">
+                <div className="logowrapper">
+                <div className="logoinit">
+                    <h1>CG</h1>
+                </div>
+                <div className="logofn">
+                    <h4>Clayborn</h4>
+                </div>
+                <div className="logoln">
+                    <h4>Guess</h4>
+                </div>
+                </div>
+
+                <div className="home2">Navbar</div>
+                <div className="home3">main</div>
+                <div className="home4">footer</div>   
             </div>
         )
     }
