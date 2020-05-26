573a70891185fc134d6aecb28c3a503d31fe69ff
ClayGuess <guessclay@Yahoo.com>
2020-05-26 18:53:17 -0400

    styling

--- client/src/components/BlogList.js
+++ client/src/components/BlogList.js
@@ -10,33 +10,81 @@ export default class BlogList extends Component {
         error: '',
         blogs: []
     }
-    componentDidMount(){
+    componentDidMount() {
         this.fetchBlogs();
     }
 
     fetchBlogs = async () => {
         try {
             const res = await axios.get('/api/v1/blogs/');
-            this.setState({blogs: res.data});
+            this.setState({ blogs: res.data });
         }
         catch (err) {
             console.log(err)
-            this.setState({error: err.message})
+            this.setState({ error: err.message })
         }
     }
     render() {
-        if (this.state.error){
+        if (this.state.error) {
             return <div>{this.state.error}</div>
         }
         return (
-            <div>
-                <h1>All Blogs</h1>
-                {this.state.blogs.map(blog => (
-                    <div key={blog.id}>
-                        <Link to={`/blog/${blog.id}`} >{blog.title}</Link>
+            <div className="blogListWrapper">
+                <div className="logowrapper">
+                    <div className="logoinit">
+                        <h1>CG</h1>
                     </div>
-                ))}
-                
+                    <div className="logofn">
+                        <h4>Clayborn</h4>
+                    </div>
+                    <div className="logoln">
+                        <h4>Guess</h4>
+                    </div>
+                </div>
+                <div className="blogListNav">
+                    <nav><div><a href="/About">About Me</a></div></nav>
+                    <nav><div><a href="/">Home</a></div></nav>
+                    <nav><div><a href="/Contact">Contact Me</a></div></nav>
+                    <nav><div><a href="/Projects">Projects</a></div></nav>
+                </div>
+                <div className="blogListMain">
+                    <div className="blogListLink">
+                        <h1>Blogs</h1>
+                        {this.state.blogs.map(blog => (
+                            <div key={blog.id}>
+                                <Link to={`/blog/${blog.id}`} >{blog.title}</Link>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="excerpt1">
+                        <p>"Gobbledygook!"</p>
+                    </div>
+                    <div className="excerpt2">
+                        <p>"More Gobbledygook!"</p>
+                    </div>
+                    <div className="excerpt3">
+                        <p>"Even more Gobbledygook!"</p>
+                    </div>
+                    <div className="excerpt4">
+                        <p>"Ok, this is geting ridiculous."</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <div><p>© Background image courtesy of Travis Clark</p></div>
+                    <div><p>Site created by Clayborn Guess using React, Django REST and PostgreSQL.</p></div>
+                        <div className="facebookIcon">
+                            <img src="/facebook-icon.png" width="35px" alt=""></img>
+                        </div>
+                        <div className="linkedinIcon">
+                            <img src="/linkedin-icon.jpg" width="35px" alt=""></img>
+                        </div>
+                        <div className="instagramIcon">
+                            <img src="/instagram-icon.png" width="35px" alt=""></img>
+                        </div>
+                        <div className="twitterIcon">
+                            <img src="/twitter-icon.png" width="35px" alt=""></img>
+                    </div>
+                </div>
             </div>
         );
     }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,30 +5,52 @@ export default class Home extends Component {
         return (
             <div className="homewrapper">
                 <div className="logowrapper">
-                <div className="logoinit">
-                    <h1>CG</h1>
-                </div>
-                <div className="logofn">
-                    <h4>Clayborn</h4>
-                </div>
-                <div className="logoln">
-                    <h4>Guess</h4>
-                </div>
+                    <div className="logoinit">
+                        <h1>CG</h1>
+                    </div>
+                    <div className="logofn">
+                        <h4>Clayborn</h4>
+                    </div>
+                    <div className="logoln">
+                        <h4>Guess</h4>
+                    </div>
                 </div>
 
                 <div className="homenav">
 
                     <nav><div><a href="/About">About Me</a></div></nav>
-                    <nav><div><a href="/Blog">Blog</a></div></nav>  
+                    <nav><div><a href="/Blog">Blog</a></div></nav>
                     <nav><div><a href="/Contact">Contact Me</a></div></nav>
-                    <nav><div><a href="/Projects">Projects</a></div></nav>  
-                        
-                   
+                    <nav><div><a href="/Projects">Projects</a></div></nav>
+
+
                 </div>
-                <div className="home3">main</div>
-                <div className="home4">
-                    <p>footer</p>
-                </div>   
+                <div className="brandStatement1">
+                    <div className="quote1">
+                        <p>“If you can't fly then run,
+                        if you can't run then walk,
+                        if you can't walk then crawl,
+                        but whatever you do you have to keep moving forward.”</p>
+                        <p>― Martin Luther King Jr.</p>
+
+                    </div>
+                </div>
+                {/* <div className="icons">
+                    <div>content</div>
+                    <div>content</div>
+                    <div className="facebookIcon">
+                        <img src="/facebook-icon.png" width="35px" alt=""></img>
+                    </div>
+                    <div className="linkedinIcon">
+                        <img src="/linkedin-icon.jpg" width="35px" alt=""></img>
+                    </div>
+                    <div className="instagramIcon">
+                        <img src="/instagram-icon.png" width="35px" alt=""></img>
+                    </div>
+                    <div className="twitterIcon">
+                        <img src="/twitter-icon.png" width="35px" alt=""></img>
+                    </div>
+                </div>    */}
             </div>
         )
     }
