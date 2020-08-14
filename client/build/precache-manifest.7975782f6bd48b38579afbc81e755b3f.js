7d1b30a6311d5b79ce1234e78fe0cfb5edb2feef
ClayGuess <guessclay@Yahoo.com>
2020-08-14 13:14:20 -0400

    edit to .gitignore

---
+++ client/build/precache-manifest.7975782f6bd48b38579afbc81e755b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368cc88c72aefa084ea926b3458a7890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c9e2e7ecd56a72f1dc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c060e2a291f2c3e79864",
+    "url": "/static/js/2.029e43b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.029e43b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c9e2e7ecd56a72f1dc",
+    "url": "/static/js/main.1b59d18e.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
