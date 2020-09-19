ae0459a4c0bf956852e3127cdf5f647715efec84
ClayGuess <guessclay@Yahoo.com>
2020-09-18 23:35:54 -0400

    styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,24 +3,29 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div className="homewrapper">
-                <div className="logowrapper">
-                    <div className="logoinit">
-                        <h1>CG</h1>
-                    </div>
-                    <div className="logofn">
-                        <h4>Clayborn</h4>
-                    </div>
-                    <div className="logoln">
-                        <h4>Guess</h4>
-                    </div>
-                </div>
-
-                <div className="homenav">
-                    <nav><div><a href="/About_me">About Me</a></div></nav>
-                    <nav><div><a href="/Blog">Blog</a></div></nav>
-                    <nav><div><a href="/Contact">Contact Me</a></div></nav>
-                    <nav><div><a href="/Projects">Projects</a></div></nav>
+            <div className="homeContainer">
+                <div className="header">
+                  <div className="navBar1">
+                      <div className="bar1Top">                         
+                        <nav><div><a href="/Resume">Resume</a></div></nav>
+                        <nav><div><a href="/Blog">Blog</a></div></nav>
+                      </div>
+                      <div className="bar1Bottom">
+                          <p>placeholder</p>
+                      </div>
+                  </div>
+                  <div className="logo1">
+                      <img src="/CG_Logo6.png" width="300" alt="Logo"/>
+                  </div>
+                  <div className="navBar2">
+                      <div className="bar2Top">
+                        <nav><div><a href="/Contact">Contact Me</a></div></nav>
+                        <nav><div><a href="/Projects">Projects</a></div></nav>
+                        </div>
+                        <div className="bar2Bottom">
+                          <p>placeholder</p>
+                      </div>
+                  </div>                    
                 </div>
                 <div className="brandStatement1">
                     <div className="quote1">
@@ -30,6 +35,14 @@ export default class Home extends Component {
                         but whatever you do you have to keep moving forward.”</p>
                         <p>― Martin Luther King Jr.</p>
 
+                    </div>
+                </div>
+                <div className="aboutMe">
+                    <div className="bioPic">
+                        <img src="/testphoto1.jpeg" height="300" alt="test"/>
+                    </div>
+                    <div className="bio">
+                        <p></p>
                     </div>
                 </div>
                 <div className="icons">
